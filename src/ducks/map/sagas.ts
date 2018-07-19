c8c37b218238ea274ddd30a4452ef5fa40cbf06f
Bernard <bero4net@gmail.com>
2018-07-19 07:42:23 +0200

    implement `fetchMarkersRequest`

--- src/ducks/map/sagas.ts
+++ src/ducks/map/sagas.ts
@@ -1,13 +1,19 @@
-import { all, call, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
 import mapTypes from './types';
+import { api } from '../../utils/api';
+import { MarkerDto } from '../../types/Dto/MarkerDto';
+import { Marker } from '../../types/Marker';
+import markerDtoToMarker from '../../utils/markerDtoToMarker';
+import { mapActions } from './action';
 
-const fetchMarkersRequesr = () => {
-  console.log('fetching markers');
-};
+const fetchMarkersRequest = () =>
+  api.get('/markers').then(res => res.data);
 
 function* fetchMarkers() {
   try {
-    yield call(fetchMarkersRequesr);
+    const markersDto: MarkerDto[] = yield call(fetchMarkersRequest);
+    const markers: Marker[] = markersDto.map(markerDto => markerDtoToMarker(markerDto));
+    yield put(mapActions.markers.add(markers));
   } catch (e) {
     console.log(e);
   }
