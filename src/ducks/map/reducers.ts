0ad9b8f1186b10df5bfdc3f77fc3862aaeaaec42
Bernard <bero4net@gmail.com>
2018-07-19 07:44:53 +0200

    implement `add marker` reducer

--- src/ducks/map/reducers.ts
+++ src/ducks/map/reducers.ts
@@ -1,17 +1,22 @@
 import { getType } from 'typesafe-actions';
 import produce from 'immer';
 import { MapAction, mapActions } from './action';
+import { Marker } from '../../types/Marker';
+import getUniqueArray from '../../utils/getUniqueArray';
 
 export type MapState = {
+  markers: Marker[],
 };
 
 export const initialState: MapState = {
+  markers: [],
 };
 
 export const mapReducer = produce<MapState, MapAction>(
   (state, action) => {
     switch (action.type) {
-      case getType(mapActions.add):
+      case getType(mapActions.markers.add):
+        state.markers = getUniqueArray<Marker>([...state.markers, ...action.payload]);
         return;
     }
   },
