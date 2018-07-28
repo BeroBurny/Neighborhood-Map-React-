0119d5e42f1fe7a70e996392179ba3c907cdd634
Bernard <bero4net@gmail.com>
2018-07-28 11:48:28 +0200

    implement `markerFilter` based on selected markers

--- src/ducks/map/selectors.ts
+++ src/ducks/map/selectors.ts
@@ -3,8 +3,23 @@ import { RootState } from '../store';
 import { SelectOption } from '../../types/SelectOption';
 import { Marker } from '../../types/Marker';
 
-export const getMapMarkers = (state: RootState): Marker[] => state.map.markers;
+export const getAllMapMarkers = (state: RootState): Marker[] => state.map.markers;
 export const getSelectedMapTypes = (state: RootState): SelectOption[] => state.map.selected;
+
+export const getMapMarkers = createSelector(
+  getAllMapMarkers,
+  getSelectedMapTypes,
+  (markers, selectedTypes) =>
+    markers.filter((marker: Marker) => {
+      let isOnList = false;
+      selectedTypes.forEach((selected: SelectOption) => {
+        if (selected.value === marker.type) {
+          isOnList = true;
+        }
+      });
+      return isOnList;
+    }),
+);
 
 export const getIfIsRestaurantVisible = createSelector(
   getSelectedMapTypes,
